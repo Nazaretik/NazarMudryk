ff1e9d6ffaf32cabeb3b2f869d2c473e53fe64c1
unknown <mudryk.n@nltu.lviv.ua>
2024-11-25 15:11:00 +0200

    first commit

---
+++ Lab 10/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <time.h>
+using name std;
+void main()
+{
+	float** matrix;
+	int rowCount, colCount;
+	cout << "Введіть розмір двовимірного масиву" << endl;
+	cout << "Кількість рядків: ";
+	cin >> rowCount;
+	cout<< "Кількість рядків: ";
+	cin >> colCount;
+
+
+	matrix = new float* [rowCount];
+
+	float min = -10, max = 50;
+	srand(time(0));
+	cout << "Початковий масив: " << endl;
+	for (int i = 0; i < rowCout; i++)
+	{
+		matrix[i] = mew float[colCount];
+
+	}
+
+}
